0a74f5668a6ba3048c8d60d6b596e7ec58d6769a
JosephShortt <g00419690@atu.ie>
2025-10-08 23:07:30 +0100

    Added basic account object creation

--- backend/src/main/java/com/josephshortt/blockchainbank/Main.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/Main.java
@@ -1,7 +1,14 @@
 package com.josephshortt.blockchainbank;
 
+import com.josephshortt.blockchainbank.models.Account;
+import com.josephshortt.blockchainbank.models.AccountType;
+import com.josephshortt.blockchainbank.models.Customer;
+
 public class Main {
     static void main() {
+        Customer joe = new Customer("c1","Joe","Shortt","josephshortt1@gmail.com");
+        Account joesCurrent = new Account("c1","a1", AccountType.CURRENT,550.45);
 
+        System.out.println(joe.getFirstName()+" "+joe.getSurname()+"\nAccount Type: "+joesCurrent.getAccountType()+"\nAccount Balance: €"+joesCurrent.getBalance());
     }
 }
--- backend/src/main/java/com/josephshortt/blockchainbank/models/Account.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/models/Account.java
@@ -1,4 +1,46 @@
 package com.josephshortt.blockchainbank.models;
 
 public class Account {
+    private String customerId,accountId;
+    private AccountType accountType;
+    private double balance;
+
+    public Account(String customerId, String accountId, AccountType accountType,double balance){
+        this.customerId=customerId;
+        this.accountId=accountId;
+        this.accountType=accountType;
+        this.balance=balance;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
