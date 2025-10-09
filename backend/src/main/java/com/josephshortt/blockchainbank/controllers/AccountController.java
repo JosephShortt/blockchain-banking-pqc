6da9a561502c8c67dd2eb2a479c94e7b1d885d56
JosephShortt <g00419690@atu.ie>
2025-10-09 15:57:26 +0100

    established connection between frontend and backend using spring boot

--- backend/src/main/java/com/josephshortt/blockchainbank/controllers/AccountController.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/controllers/AccountController.java
@@ -5,13 +5,12 @@ import com.josephshortt.blockchainbank.models.Customer;
 
 @RestController
 @RequestMapping("/api/accounts")
-@CrossOrigin(origins = "http://localhost:3000") // allow your React app to call this API
+@CrossOrigin(origins = "http://localhost:3000")
 public class AccountController {
 
     @PostMapping
     public Customer createAccount(@RequestBody Customer customer) {
         System.out.println("Received new customer: " + customer.getFirstName());
-        // In future: save to DB or blockchain ledger
-        return customer; // send it back to frontend (optional)
+        return customer;
     }
 }
