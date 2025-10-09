9663465f874ee9d4db2d1ffee6e4afa5ef252fdc
JosephShortt <g00419690@atu.ie>
2025-10-09 15:49:08 +0100

    Established a connection between frontend and backend using spring boot

--- backend/src/main/java/com/josephshortt/blockchainbank/Main.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/Main.java
@@ -1,14 +1,17 @@
 package com.josephshortt.blockchainbank;
 
+import com.josephshortt.blockchainbank.controllers.AccountController;
 import com.josephshortt.blockchainbank.models.Account;
 import com.josephshortt.blockchainbank.models.AccountType;
 import com.josephshortt.blockchainbank.models.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
-    static void main() {
-        Customer joe = new Customer("c1","Joe","Shortt","josephshortt1@gmail.com");
-        Account joesCurrent = new Account("c1","a1", AccountType.CURRENT,550.45);
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
 
-        System.out.println(joe.getFirstName()+" "+joe.getSurname()+"\nAccount Type: "+joesCurrent.getAccountType()+"\nAccount Balance: €"+joesCurrent.getBalance());
+
     }
 }
