3d7ad82319e6bcaf4e23f50ac6f40389d2660ed9
JosephShortt <g00419690@atu.ie>
2025-10-10 00:47:11 +0100

    Can log into customer account and fetches customer and bank details to display in react web app

--- frontend/src/components/UserLogin.js
+++ frontend/src/components/UserLogin.js
@@ -5,9 +5,8 @@ function UserLogin() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [userData, setUserData] = useState(null);  // 👈 this fixes your error
-
-
+    const [userData, setUserData] = useState(null);
+    const [accountData, setAccountData] = useState(null);
     const navigate = useNavigate();
 
     async function handleSubmit(e) {
@@ -19,9 +18,9 @@ function UserLogin() {
             });
 
             // 200 OK, login successful
-            setUserData(response.data);
-
-            console.log("Login successful:", userData);
+            setUserData(response.data.customer);
+            setAccountData(response.data.bankAccount)
+            console.log("Login successful:", response.data);
 
 
         } catch (error) {
@@ -59,11 +58,19 @@ function UserLogin() {
                 <input type="submit" />
             </form>
 
-            {userData && (
+            {userData && accountData && (
                 <div>
                     <h2>Welcome, {userData.firstName}!</h2>
                     <p>Email: {userData.email}</p>
                     <p>Customer ID: {userData.customerId}</p>
+
+                    <br />
+
+                    <p><strong> Account Details</strong></p>
+                    <p>Account ID: {accountData.accountId}</p>
+                    <p>Account Type: {accountData.accountType}</p>
+                    <p>Balance: {accountData.balance}</p>
+
                 </div>
             )}
         </div>
