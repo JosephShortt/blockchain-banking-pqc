224955a0d219edabdd325f1a64f6d254f43237c3
JosephShortt <g00419690@atu.ie>
2025-10-09 17:36:11 +0100

    Refactored functions to be more decriptive and added functionality to create default bank account linked to customer accoount when customer account is created

--- backend/src/main/java/com/josephshortt/blockchainbank/Main.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/Main.java
@@ -1,9 +1,5 @@
 package com.josephshortt.blockchainbank;
 
-import com.josephshortt.blockchainbank.controllers.AccountController;
-import com.josephshortt.blockchainbank.models.Account;
-import com.josephshortt.blockchainbank.models.AccountType;
-import com.josephshortt.blockchainbank.models.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
