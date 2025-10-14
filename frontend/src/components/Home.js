2dad6704c7754989bda407bf9d783d48fbfc312b
JosephShortt <g00419690@atu.ie>
2025-10-14 22:35:03 +0100

    Added functionality around logging in directing user to home with account info and account creation measure to avoid duplicate account(same email) to ensure unique accounts

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,67 +1,22 @@
 import { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
+import { useUser } from "../contexts/UserContext";
 
 function Home() {
 
-  const [accountData = [], setAccountData] = useState();
+  const { userData, accountData } = useUser();
 
-  useEffect(() => {
-    async function fetchAccountDetails() {
-      try {
-        const response = await axios.get("http://localhost:8080/api/accounts");
-        console.log("Fetched accounts:", response.data);
-        setAccountData(response.data)
+   if (!userData) return <p>Please log in.</p>;
 
-      } catch (err) {
-        console.error("Error fetching accounts:", err);
-      }
-    }
-    fetchAccountDetails();
-  }, []);
-
-  const accounts = accountData.map((item) =>
-    <div key={item.customerId}>
-      <div style={{border: '3px solid rgba(0, 0, 0, 0.35)', margin:'10px', padding:'5px'}} >
-        <p>
-          <b>Customer ID: {item.customerId}</b>
-        </p>
-
-        <p>
-          <b>Name: {item.firstName} {item.surname}</b>
-        </p>
-        <p>
-          <b>Email: {item.email}</b>
-        </p>
-        <p>
-          <b>Password: {item.password}</b>
-        </p>
-        <br />
-      </div>
-      
+return (
+    <div>
+      <h2>Welcome, {userData.firstName}!</h2>
+      <p>Email: {userData.email}</p>
+      <p>Account ID: {accountData.accountId}</p>
+      <p>Balance: {accountData.balance}</p>
     </div>
-
-  )
-
-
-
-  console.log(accounts)
-
-  return (
-    <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      
-    }}>
-      <div>
-        {accounts}
-
-      </div>
-    </div >
-
-  )
-
+  );
 };
 
 export default Home;
