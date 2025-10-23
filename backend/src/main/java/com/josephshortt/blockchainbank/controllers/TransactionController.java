a7e777d6832c84332a0ff4e28b2975118f3a0ccf
JosephShortt <g00419690@atu.ie>
2025-10-23 21:47:26 +0100

    users and accounts are persistent

--- backend/src/main/java/com/josephshortt/blockchainbank/controllers/TransactionController.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/controllers/TransactionController.java
@@ -1,10 +1,10 @@
 package com.josephshortt.blockchainbank.controllers;
 
+import com.josephshortt.blockchainbank.models.LoadBankAccounts;
 import com.josephshortt.blockchainbank.models.TransactionRequest;
 import com.josephshortt.blockchainbank.models.DefaultBankAccount;
 import org.springframework.web.bind.annotation.*;
 
-import static com.josephshortt.blockchainbank.controllers.CustomerAccountController.defaultBankAccounts;
 
 @RestController
 @RequestMapping("/api/accounts/transaction")
@@ -19,7 +19,7 @@ public class TransactionController {
         double amount = request.getAmount();
         String iban = request.getIban();
 
-        for (DefaultBankAccount bank : defaultBankAccounts) {
+        for (DefaultBankAccount bank : LoadBankAccounts.defaultBankAccounts) {
 
             if (bank.getIban().equals(request.getIban())) {
                 bank.setBalance(bank.getBalance() + amount);
---
+++ backend/src/main/java/com/josephshortt/blockchainbank/models/LoadBankAccounts.java
@@ -0,0 +1,36 @@
+    package com.josephshortt.blockchainbank.models;
+
+    import com.josephshortt.blockchainbank.repository.BankAccountRepository;
+    import jakarta.annotation.PostConstruct;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Component;
+
+    import java.util.ArrayList;
+    import java.util.List;
+
+    @Component
+    public class LoadBankAccounts {
+        public static final List<DefaultBankAccount> defaultBankAccounts = new ArrayList<>();
+
+        @Autowired
+        private BankAccountRepository bankAccountRepository;
+
+        @PostConstruct
+        public void loadBankAccounts(){
+
+            List<DefaultBankAccount> allAccounts = bankAccountRepository.findAll();
+            defaultBankAccounts.clear();
+            defaultBankAccounts.addAll(allAccounts);
+
+            System.out.println("Loaded " + defaultBankAccounts.size() + " bank accounts from database.");
+            for(DefaultBankAccount account : defaultBankAccounts){
+                System.out.println("ID: " + account.getCustomerId() + " Balance: " + account.getBalance());
+
+            }
+        }
+
+        public List<DefaultBankAccount> getAccounts() {
+            return defaultBankAccounts;
+        }
+
+    }
