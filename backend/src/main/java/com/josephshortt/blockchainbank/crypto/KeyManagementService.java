228bb21e3d85ebd44262605976c94055bcf7b1f9
JosephShortt <g00419690@atu.ie>
2026-02-06 23:35:26 +0000

    changed dilithium from 3 to 5 for higher securty slower processing

--- backend/src/main/java/com/josephshortt/blockchainbank/crypto/KeyManagementService.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/crypto/KeyManagementService.java
@@ -1,4 +1,11 @@
 package com.josephshortt.blockchainbank.crypto;
 
 public class KeyManagementService {
+    /*
+    * KeyManagementService class is responsible for:
+    * 1. Generating keys when user registers (dilithium public-private)
+    * 2. Encrypting keys with user password
+    * 3. Storing encrypted keys in DB
+    * 4. Decrypting keys when user requests a transaction
+    * */
 }
