c710a981e681015bae1218fe4be0fab40d03f5a8
JosephShortt <g00419690@atu.ie>
2026-02-19 00:17:42 +0000

    Defind methods to be implemented for blockchain service

--- backend/src/main/java/com/josephshortt/blockchainbank/blockchain/BlockchainService.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/blockchain/BlockchainService.java
@@ -7,6 +7,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @Service
 public class BlockchainService {
     @Autowired
@@ -18,6 +23,13 @@ public class BlockchainService {
     @Autowired
     private PQCService pqcService;
 
+    /*
+      Basic Block operations
+      1. Initialise genesis block
+      2. calculate block hash
+      3. Get latest block in chain for creating new blocks
+      4. get pending transactions for block creation
+     */
     //If blockchain has not been initialised create the genesis block(block 1)
     @PostConstruct
     public void initialiseChain() throws Exception {
@@ -38,5 +50,94 @@ public class BlockchainService {
     public String calculateHash(Block block) throws Exception {
         String data = block.getPrevHash() + block.getBlockNumber() + block.getMerkleRoot() + block.getCreatedAt();
         return pqcService.hashSHA256(data);
+    }
+
+    public Block getLatestBlock(){
+
+        return null;
+    }
+
+    //get pending transactions
+    public List<BlockTransaction> getPendingTransactions(){
+
+        return List.of();
+    }
+
+    /*
+    Block creation methods
+    1. createBlock()
+    2. calculate merkle root from lis of transactions
+    3. sign block
+     */
+
+    public void createBlock(){
+
+    }
+
+
+    private String calculateMerkleRoot(List<BlockTransaction> transactions){
+
+        return "";
+    }
+
+
+    private String signBlock(Block block){
+
+        return "";
+    }
+
+      /*
+    Validation and consensus
+    1. validate block
+    2. validate chain
+    3. validate transaction sig
+
+     */
+
+    public boolean validateBlock(Block block){
+
+        return false;
+    }
+
+    public boolean validateChain(){
+
+        return false;
+    }
+
+    /*
+    Settlement methods (After consensus)
+    1. Process Incoming transactions
+    2. Calculate Net settlement
+    3. Settle Bank reserves
+     */
+
+    public void processIncomingTransactions(Block finalizedBlock){
+
+    }
+
+    public Map<String, BigDecimal> calculateNetPositions(Block block){
+
+        return Map.of();
+    }
+
+    public void settleBankReserves(Block block){}
+
+    /*
+    Utility Methods
+    1. get current bank id
+    2. Count blocks
+    3. Get block by number
+     */
+
+    private String getCurrentBankId(){
+        return "";
+    }
+
+    public long getBlockCount(){
+        return 0;
+    }
+
+    public Optional<Block> getBlockByNumber(Long blockNumber){
+        return Optional.empty();
     }
 }
