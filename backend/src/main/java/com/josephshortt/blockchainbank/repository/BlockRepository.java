9aaaea1fd4016d2684768cd73051daae3b66ba2f
JosephShortt <g00419690@atu.ie>
2026-02-20 00:12:54 +0000

    added manual endpoint to validate blocks by number

--- backend/src/main/java/com/josephshortt/blockchainbank/repository/BlockRepository.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/repository/BlockRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface BlockRepository extends JpaRepository<Block,Long> {
     Optional<Block> findFirstByOrderByBlockNumberDesc();
+    Optional<Block> findByBlockNumber(Long blockNumber);
 }
