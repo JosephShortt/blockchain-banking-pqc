e5c51b517c9adc6a098a296c60471199c107d7c5
JosephShortt <g00419690@atu.ie>
2026-02-23 19:57:23 +0000

    implemented validate chain method in blockchain service

--- backend/src/main/java/com/josephshortt/blockchainbank/repository/BlockRepository.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/repository/BlockRepository.java
@@ -1,11 +1,14 @@
 package com.josephshortt.blockchainbank.repository;
 
 import com.josephshortt.blockchainbank.blockchain.Block;
+import com.josephshortt.blockchainbank.blockchain.BlockStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BlockRepository extends JpaRepository<Block,Long> {
     Optional<Block> findFirstByOrderByBlockNumberDesc();
     Optional<Block> findByBlockNumber(Long blockNumber);
+    List<Block> findByStatusOrderByBlockNumberAsc(BlockStatus status);
 }
