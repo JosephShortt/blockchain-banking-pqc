be3786e87415640fbdc0547749dd3ec112f5a308
JosephShortt <g00419690@atu.ie>
2026-04-09 22:27:50 +0100

    Tx list improvement

--- backend/src/main/java/com/josephshortt/blockchainbank/models/DefaultBankAccount.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/models/DefaultBankAccount.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import static com.josephshortt.blockchainbank.models.AccountType.CURRENT;
 @Entity
 public class DefaultBankAccount {
+
     @Id
     private Long customerId;
     private String accountId,iban;
