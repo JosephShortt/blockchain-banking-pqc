12f59597d4cc9bd435f278c67c6d691d7ed72f63
JosephShortt <g00419690@atu.ie>
2026-04-09 22:50:00 +0100

    block detials available from tx history

--- backend/src/main/java/com/josephshortt/blockchainbank/repository/BlockTransactionRepository.java
+++ backend/src/main/java/com/josephshortt/blockchainbank/repository/BlockTransactionRepository.java
@@ -3,9 +3,13 @@ package com.josephshortt.blockchainbank.repository;
 import com.josephshortt.blockchainbank.blockchain.BlockTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface BlockTransactionRepository extends JpaRepository<BlockTransaction, Long> {
     List<BlockTransaction> findByBlockIsNull();
     List<BlockTransaction> findByBlockBlockNumber(Long blockNumber);
+    Optional<BlockTransaction> findBySenderIbanAndReceiverIbanAndAmount(String senderIban, String receiverIban, BigDecimal amount);
+
 }
